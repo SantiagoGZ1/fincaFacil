d50bce2e5e0a1df6e81bed942ce57656cb238789
Santiago Garcia <santi10gz10@gmail.com>
2025-02-15 18:47:11 -0500

    Eureka configuration done

---
+++ configServer/src/test/java/com/msa/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
