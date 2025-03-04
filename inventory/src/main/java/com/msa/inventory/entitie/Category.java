2de75aef7aba3b99b10cdd8719eea8704122f5cf
Santiago Garcia <santi10gz10@gmail.com>
2025-03-03 21:54:42 -0500

    logic default categories add

--- inventory/src/main/java/com/msa/inventory/entitie/Category.java
+++ inventory/src/main/java/com/msa/inventory/entitie/Category.java
@@ -12,6 +12,16 @@ public class Category {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @Column(nullable = false, unique = true)
   private String name;
   private String description;
+  @Column(nullable = false)
+  private boolean dFault = false;
+
+  public Category(String name, String description, boolean dFault) {
+    this.name = name;
+    this.description = description;
+    this.dFault = dFault;
+  }
+
 }
--- inventory/src/main/java/com/msa/inventory/repository/CategoryRepo.java
+++ inventory/src/main/java/com/msa/inventory/repository/CategoryRepo.java
@@ -3,5 +3,9 @@ package com.msa.inventory.repository;
 import com.msa.inventory.entitie.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepo extends JpaRepository<Category, Long> {
+  boolean existsByName(String name);
+  Optional<Category> findByName(String name);
 }
